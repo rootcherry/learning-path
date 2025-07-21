05e88bb06295637e1c2490748e10e3e58365452b
Chris N <hi.chrisn@gmail.com>
2025-07-21 14:40:43 -0300

    added exercise space complexity example

---
+++ Section 3 - Big O/51. Exercise: Space Complexity/code.js
@@ -0,0 +1,17 @@
+function boooooo(n) {
+  for (let i = 0; i < n.length; i++) {
+    console.log("boooooo!");
+  }
+}
+
+boooooo([1, 2, 3, 4, 5]); // O(1)
+
+function arrayOfHiNTimes(n) {
+  let hiArray = [];
+  for (let i = 0; i < n; i++) {
+    hiArray[i] = "hi";
+  }
+  return hiArray;
+}
+
+console.log(arrayOfHiNTimes(6)); // O(n)
