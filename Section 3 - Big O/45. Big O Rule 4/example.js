5447e20004666955a6bea61848c0868bbef6ec05
Chris N <hi.chrisn@gmail.com>
2025-07-21 12:53:21 -0300

    added Rule 4: Drop Non Dominants/ Non Dominant Terms example

---
+++ Section 3 - Big O/45. Big O Rule 4/example.js
@@ -0,0 +1,17 @@
+// Rule 4: Drop Non Dominants/ Non Dominant Terms
+
+function printAllNumbersThenAllPairSums(numbers) {
+  console.log("these are the numbers:");
+  numbers.forEach(function(number) {
+    console.log(number);
+  });
+
+  console.log("and these are their sums:");
+  numbers.forEach(function(firstNumber) {
+    numbers.forEach(function(secondNumber) {
+      console.log(firstNumber + secondNumber);
+    });
+  });
+}
+
+printAllNumbersThenAllPairSums([1, 2, 3, 4, 5]); // O(n^2)
---
+++ Section 3 - Big O/45. Big O Rule 4/forEach.js
@@ -0,0 +1,7 @@
+const array = ["a", "b", "c"];
+
+array.forEach((element) => console.log(element));
+
+// Expected output: "a"
+// Expected output: "b"
+// Expected output: "c"
