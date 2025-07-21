27ee0909233b596346f6753742486b80409ab761
Chris N <hi.chrisn@gmail.com>
2025-07-20 22:38:12 -0300

    added Rule 3: Different terms for inputs example

---
+++ Section 3 - Big O/43. Big O Rule 3/example.js
@@ -0,0 +1,13 @@
+// Rule 3: Different terms for inputs
+
+function compressAllBoxes(boxes, boxes2) {
+    boxes.forEach(function(boxes){
+        console.log(boxes);
+    });
+
+    boxes2.forEach(function(boxes) {
+        console.log(boxes);
+    });
+}
+
+// O(a + b)
