5f161c186271316bf1121883ab8e4922b9c47594
Chris N <hi.chrisn@gmail.com>
2025-07-21 12:38:20 -0300

    andrei solution

--- Section 3 - Big O/44. O(n^2)/logAllPairsOfArray.js
+++ Section 3 - Big O/44. O(n^2)/logAllPairsOfArray.js
@@ -8,5 +8,31 @@ for (i = 0; i < boxes.length; i++) {
     for (j = 0; j < boxes.length; j++) {
         arr.push(boxes[i], boxes[j]);
     }
-    console.log(arr.join(','));   
+    //console.log(arr.join(','));   
 }
+
+// andrei solution
+const boxes2 = ['a', 'b', 'c', 'd', 'e'];
+
+function logAllPairsOfArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            console.log(array[i], array[j]);
+        }
+    }
+}
+
+// logAllPairsOfArray(boxes2);
+
+// ES5
+function logAllPairsOfArray2(array) {
+    boxes2.forEach(function(firstBox) {
+        boxes2.forEach(function(secondBox) {
+            console.log(firstBox, secondBox);
+        })
+    })
+}
+
+logAllPairsOfArray2(boxes2);
+// O(n * n) -> O(n^2)
+// O(n2) - Quadratic Time
