6b47ca17edcb3b4b8c2606afbbfad52249e12336
Chris N <hi.chrisn@gmail.com>
2025-07-22 19:40:10 -0300

    added pop method

--- Section 6 - Arrays/73. Implementing an Array/index.js
+++ Section 6 - Arrays/73. Implementing an Array/index.js
@@ -13,10 +13,19 @@ class MyArray {
     this.length++;
     return this.length;
   }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
 }
 
 const newArray = new MyArray();
 
 newArray.push("hi");
 newArray.push("you");
+newArray.push("!");
+newArray.pop();
 console.log(newArray);
