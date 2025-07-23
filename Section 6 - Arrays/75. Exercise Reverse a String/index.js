a60ca16ff660a9b238739b7e975d26c30d80ff79
Chris N <hi.chrisn@gmail.com>
2025-07-23 10:25:35 -0300

    added solution

--- Section 6 - Arrays/75. Exercise Reverse a String/index.js
+++ Section 6 - Arrays/75. Exercise Reverse a String/index.js
@@ -12,4 +12,37 @@ function reverse(str) {
   console.log(output.join(""));
 }
 
-reverse(str);
+//reverse(str);
+
+// Andrei solution
+function reverse2(str) {
+  // check input
+  if (!str || str.length < 2 || typeof str != "string") {
+    return;
+    ("hmm that's not good");
+  }
+
+  const backwards = [];
+  const totalItems = str.length - 1;
+
+  for (let i = totalItems; i >= 0; i--) {
+    backwards.push(str[i]);
+  }
+
+  console.log(backwards);
+
+  return backwards.join("");
+}
+
+function reverse3(str) {
+  return str.split("").reverse().join("");
+}
+
+const reverse4 = (str) => str.split("").reverse().join("");
+
+const reverse5 = (str) => [...str].reverse().join("");
+
+console.log(reverse2("Hi My name is Andrei"));
+console.log(reverse3(" ,olleH"));
+console.log(reverse4("dlroW"));
+console.log(reverse5("👩‍💻 !!!"));
