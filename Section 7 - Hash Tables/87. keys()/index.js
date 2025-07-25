6f53701b3e1682d7af8caaf9297142e3a09aed9e
Chris N <hi.chrisn@gmail.com>
2025-07-24 21:26:32 -0300

    added keys method example

---
+++ Section 7 - Hash Tables/87. keys()/index.js
@@ -0,0 +1,54 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+    // this.data = [];
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  } //O(1)
+
+  get(key) {
+    const address = this._hash(key);
+    const currentBucket = this.data[address];
+    console.log(currentBucket);
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    } // O(1) if no collisions
+    return undefined;
+  }
+
+  keys() {
+    const keysArray = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        //console.log(this.data[i][0][0]);
+        keysArray.push(this.data[i][0][0]);
+      }
+    }
+    return keysArray;
+  }
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set("grapes", 10000);
+myHashTable.set("apples", 9);
+myHashTable.set("oranges", 2);
+console.log(myHashTable.keys());
