b65ef896f6cdc097a1b602979687d89c4e45e337
Chrisn N <hi.chrisn@gmail.com>
2025-07-28 17:39:57 -0300

    pomodoro time

--- Section_8_Linked_Lists/100._Our_First_Linked_List/index.js
+++ Section_8_Linked_Lists/100._Our_First_Linked_List/index.js
@@ -46,6 +46,39 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  insert(index, value) {
+    const newNode = new Node(value);
+    if (index === 0) {
+      this.prepend(value);
+      return this;
+    }
+
+    let node = this.head;
+    let counter = 0;
+
+    while (node != null) {
+      if (counter === index - 1) {
+        newNode.next = node.next;
+        node.next = newNode;
+        this.length++;
+        break;
+      }
+      counter++;
+      node = node.next;
+    }
+    return this;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode != null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
@@ -53,5 +86,6 @@ const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
-
-console.log(myLinkedList);
+myLinkedList.insert(2, 99);
+console.log(myLinkedList.printList());
+// console.log(myLinkedList);
