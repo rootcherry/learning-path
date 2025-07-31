a20934b0c35e40ddba92a86955cc615333529c54
Chrisn N <hi.chrisn@gmail.com>
2025-07-31 13:51:05 -0300

    added lookup method

--- Section_10_Trees/136._Exercise_Binary_Search_Tree/solution.js
+++ Section_10_Trees/136._Exercise_Binary_Search_Tree/solution.js
@@ -37,7 +37,23 @@ class BinarySearchTree {
     }
   }
 
-  lookup(value) {}
+  lookup(value) {
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode;
+      }
+    }
+    return false;
+  }
 
   remove() {}
 }
@@ -51,7 +67,8 @@ tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
-console.log(JSON.stringify(traverse(tree.root)));
+console.log(tree.lookup(9));
+// console.log(JSON.stringify(traverse(tree.root)));
 
 //       9
 //   4       20
